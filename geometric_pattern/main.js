cacfd924e0674ca7a01a27586313cdda9cc37c3a
Cayce Beames <case@blackalchemy.to>
2017-02-18 14:45:13 +0000

    Added jarring animation to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,20 +1,26 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
+var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
   createCanvas(480, 600);
+  
+  frameRate(5);
+  
   circleDiameter = width/NUM_CIRCLES;
   circleRadius = circleDiameter/2;
-}
-
-function draw() {
+  
   rVal = 255;
   gVal = 0;
   bVal = 0;
-  
+}
+
+function draw() {
   var isShifted = false;
   
   var y = height;
@@ -28,8 +34,8 @@ function draw() {
     }
     
     while (x <= width) {
-      fill(color(rVal, bVal, gVal));
       stroke(color(rVal, bVal, gVal)); 
+      fill(color(rVal, bVal, gVal));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
@@ -37,9 +43,9 @@ function draw() {
     y = y - circleRadius;
     isShifted = !isShifted;
     
-    rVal = rVal -2;
-    gVal = gVal + 7;
-    bVal = bVal +3;
+    rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) %256;
   }
   
   function keyPressed() {
