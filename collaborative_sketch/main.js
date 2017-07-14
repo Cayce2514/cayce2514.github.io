50facfcd604f141793e0640346f1f7daad34a8be
Cayce Beames <case@blackalchemy.to>
2017-07-14 05:00:59 +0000

    created bulk of sketchpad

--- collaborative_sketch/main.js
+++ collaborative_sketch/main.js
@@ -1,12 +1,46 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCA7_xKMf6oNW8WpiLc77rqCfgULyIoW-I",
-    authDomain: "collaborative-sketch-a815a.firebaseapp.com",
-    databaseURL: "https://collaborative-sketch-a815a.firebaseio.com",
-    projectId: "collaborative-sketch-a815a",
-    storageBucket: "",
-    messagingSenderId: "848954033921"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyCA7_xKMf6oNW8WpiLc77rqCfgULyIoW-I",
+  authDomain: "collaborative-sketch-a815a.firebaseapp.com",
+  databaseURL: "https://collaborative-sketch-a815a.firebaseio.com",
+  projectId: "collaborative-sketch-a815a",
+  storageBucket: "",
+  messagingSenderId: "848954033921"
+};
+
+firebase.initializeApp(config);
+ 
+var pointsData = firebase.database().ref();
+var points = [];
+
+function setup() {
+  var canvas = createCanvas(400, 400);
+  background(255);
+  fill(0);
   
-  var pointsData = firebase.database().ref();
+    pointsData.on("child_added", function (point) {
+    points.push(point.val());
+  });
+  
+  canvas.mousePressed(drawPoint);
+  canvas.mouseMoved(drawPointIfMousePressed);
+}
+
+function draw() {
+  background(255);
+
+  for (var i = 0; i < points.length; i++) {
+    var point = points[i];
+    ellipse(point.x, point.y, 5, 5);
+}
+  
+function drawPoint() {
+  pointsData.push({x: mouseX, y: mouseY});
+}
+
+function drawPointIfMousePressed() {
+  if (mouseIsPressed) {
+    drawPoint();
+  }
+}
+}
