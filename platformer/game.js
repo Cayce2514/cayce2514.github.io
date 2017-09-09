153a6c941c4f91cd5f369ede968ffbe1ce73d374
Cayce Beames <case@blackalchemy.to>
2017-09-09 03:45:02 +0000

    .

--- platformer/game.js
+++ platformer/game.js
@@ -13,7 +13,7 @@ function setup() {
     isGameOver = false;
     score = 0;
 
-    createCanvas(windowHeight,windowWidth)
+    createCanvas(400,300)
     background(150, 200, 250);
     groundSprites = new Group();
 
