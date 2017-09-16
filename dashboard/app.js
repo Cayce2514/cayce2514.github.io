b946ee9c2779499445b3f630751d53c021367d8c
Cayce Beames <case@blackalchemy.to>
2017-09-16 20:37:15 +0000

    created dashboard, added things.

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,9 +1,9 @@
 function loadDate() {
-  var currentDate = new Date();
+  var currentDate = new Date(); 
   var dateString = currentDate.toString()
-    .split(" ")
-    .splice(0, 4) // making the string contain only the first four words
-    .join(" ");
+                     .split(" ") 
+                     .splice(0, 4) // making the string contain only the first four words
+                     .join(" "); 
 
   $("#date").text(dateString);
 }
@@ -13,14 +13,13 @@ function loadWeather() {
   var url = "https://api.forecast.io/forecast/"; // Dark Sky API url
   var apiKey = "73184cf35b05f08df85a58bfd68cfc0c"; // API key from Dark Sky
 
-
   function success(position) {
     var latitude = position.coords.latitude; // latitude using geolocation
     var longitude = position.coords.longitude; // longitude using geolocation
 
-  // API request:
+    // API request:
     $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-      weather.text("Based on your current location, it is " + data.currently.temperature + "\u00B0F right now and wind is bearing " + data.currently.windBearing + " at " + data.currently.windSpeed + "mph.");
+      weather.text("Based on your current location, the current weather conditions are:\r\n" + "Temperature: " + data.currently.temperature + "° F\r\nHumidity: " + data.currently.humidity + "in. Hg.");
     });
   }
 
@@ -36,28 +35,51 @@ function loadWeather() {
   weather.text("fetching weather...");
 }
 
-function loadNews() {
-  var news = $("#news");
+function loadNews1() {
+  var news = $("#news1");
   var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey="; // News API url
   var apiKey = "2a8e891933ad4f5aba875888a4f2ed1f"; // API key from News API
-
 
   $.getJSON(url + apiKey, function(data) {
 
     // map() method to call article urls and titles
+
     var titles = data.articles.map(function(articles) {
       return "<a href='" + articles.url + "'>" + articles.title + "</a>";
     });
 
-    // joining the titles with two line breaks
+     // joining the titles with two line breaks
+
     news.html(titles.join("<br><br>"));
   });
 
   // the text that will be displayed while the function is making the request
-  news.text("fetching news...");
+  news.text("fetching news..."); 
 }
 
-// Call the above functions
+function loadNews2() {
+  var news = $("#news2");
+  var url = "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey="; // News API url
+  var apiKey = "2a8e891933ad4f5aba875888a4f2ed1f"; // API key from News API
+
+  $.getJSON(url + apiKey, function(data) {
+
+    // map() method to call article urls and titles
+
+    var titles = data.articles.map(function(articles) {
+      return "<a href='" + articles.url + "'>" + articles.title + "</a>";
+    });
+
+     // joining the titles with two line breaks
+
+    news.html(titles.join("<br><br>"));
+  });
+
+  // the text that will be displayed while the function is making the request
+  news.text("fetching news..."); 
+}
+
 loadDate();
 loadWeather();
-loadNews();
+loadNews1();
+loadNews2();
