d53c5a7b29a16eeff09cb494997e99491f2dc833
cayce2514 <case@blackalchemy.to>
2017-10-25 18:58:45 +0700

    .

--- platformer/game.js
+++ platformer/game.js
@@ -13,7 +13,7 @@ function setup() {
     isGameOver = false;
     score = 0;
 
-    createCanvas(600,500)
+    createCanvas(1200,1024)
     background(150, 200, 250);
     groundSprites = new Group();
 
